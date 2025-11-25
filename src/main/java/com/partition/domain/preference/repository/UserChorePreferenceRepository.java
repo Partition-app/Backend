e8d031b42265083cb2112f7a83e96cc192a50c7e
이지원 <daggero@naver.com>
2025-11-26 07:42:46 +0900

    feat: user chore preference API

--- src/main/java/com/partition/domain/preference/repository/UserChorePreferenceRepository.java
+++ src/main/java/com/partition/domain/preference/repository/UserChorePreferenceRepository.java
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface UserChorePreferenceRepository extends JpaRepository<UserChorePreference, Long> {
     // 유저의 기존 선호도 데이터를 모두 삭제 (재등록 시 초기화용)
-    @Modifying // [추가] 데이터 변경 작업임을 명시
-    @Transactional // [추가] 트랜잭션 필수 (삭제 도중 실패하면 롤백)
+    @Modifying
+    @Transactional
     void deleteByUserId(Long userId);
 }
